31a416c418fc451675183a46ad2f5869f2251fa3
killbozer <35951632+killbozer@users.noreply.github.com>
2018-10-13 14:32:04 +0930

    treated var as being in for loop

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ function filter1() {
     var classes = document.getElementsByClassName('result');
     console.log(classes);
     for (var index = 0; index < classes.length; index++) {
-        if (classes.search("Charles Darwin")) {
+        if (classes[index].includes("Charles Darwin")) {
             console.log("its fine" + index);
         }
 
