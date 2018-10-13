fd6aa4c7e4519f37f64ba443fd92dcb40b3eff6f
killbozer <35951632+killbozer@users.noreply.github.com>
2018-10-13 14:29:33 +0930

    different substring finder

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ function filter1() {
     var classes = document.getElementsByClassName('result');
     console.log(classes);
     for (var index = 0; index < classes.length; index++) {
-        if (classes.indexOf("Charles Darwin")) {
+        if (classes.search("Charles Darwin")) {
             console.log("its fine" + index);
         }
 
